c07b6e81f300eabb9cec64a9faa58368b97f9f0e
G <a.gumeniuc@gmail.com>
2026-01-16 16:28:14 +0200

    Updated docs and examples

--- examples/actions/foxcon_set_subject_mode/action.tf
+++ examples/actions/foxcon_set_subject_mode/action.tf
@@ -1,6 +1,63 @@
+variable "subject_name" {
+  default = "test"
+}
+
+locals {
+  json_schema = <<-EOT
+{
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "$id": "http://example.com/myURI.schema.json",
+  "title": "SampleRecord",
+  "description": "Sample schema to help",
+  "type": "object",
+  "additionalProperties": false,
+  "properties": {
+    "myField1": {
+      "type": "integer",
+      "description": "The integer type is used for integral numbers."
+    }
+  }
+}
+EOT
+}
+
+resource "confluent_subject_mode" "ro" {
+  subject_name = var.subject_name
+  mode         = "READONLY"
+}
+
+resource "confluent_schema" "this" {
+
+  depends_on = [confluent_subject_mode.ro]
+
+  subject_name = var.subject_name
+
+  format = "JSON"
+  schema = jsonencode(local.json_schema)
+
+  lifecycle {
+    action_trigger {
+      events  = [after_create, after_update]
+      actions = [action.foxcon_set_subject_mode.ro]
+    }
+    action_trigger {
+      events  = [before_create, before_update]
+      actions = [action.foxcon_set_subject_mode.rw]
+    }
+  }
+
+}
+
 action "foxcon_set_subject_mode" "ro" {
   config {
-    subject_name = "test"
+    subject_name = var.subject_name
     mode         = "READONLY"
+  }
+}
+
+action "foxcon_set_subject_mode" "rw" {
+  config {
+    subject_name = var.subject_name
+    mode         = "READWRITE"
   }
 }
